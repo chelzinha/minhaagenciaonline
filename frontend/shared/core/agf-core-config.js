8f18b7906074c86f5c1a8128a5548503820334a0
chelzinha <137534302+chelzinha@users.noreply.github.com>
2026-09-22 23:29:21 -0300

    feat(agf-core): adiciona configuração compartilhada da API

---
+++ frontend/shared/core/agf-core-config.js
@@ -0,0 +1,6 @@
+/* Configuração compartilhada do AGF Core.
+ * Substituir apiUrl após o primeiro deploy do Worker agf-core-api.
+ */
+window.AGF_CORE_CONFIG = Object.freeze({
+  apiUrl: 'https://PREENCHER-AGF-CORE-API.workers.dev'
+});
